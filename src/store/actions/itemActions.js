636a868efdd9353ecf4068f8da37082b4ac410d7
kumarmukesh91 <kumarmukesh91@outlook.in>
2020-05-03 06:43:26 +0530

    Added JSONBIN

--- src/store/actions/itemActions.js
+++ src/store/actions/itemActions.js
@@ -5,7 +5,9 @@ import axios from 'axios';
 export const getItems = () => async (dispatch) => {
   dispatch({ type: LOADING_ITEMS });
   try {
-    const { data } = await axios.get('./data/cart.json');
+    const { data } = await axios.get(
+      'https://api.npoint.io/0c76acc63b2a5fa2d46b'
+    );
 
     dispatch({
       type: SET_ITEMS,

e437b04d5b9b4c18a6a000b04eae192d45ca9fc5
kumarmukesh91 <kumarmukesh91@outlook.in>
2020-05-03 06:12:48 +0530

    Updates

---
+++ precache-manifest.4ffda111d94bff572cc3ed1b06c7f64a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88a5fab666aad7f5c597d45c61cc8adf",
+    "url": "/add_to_cart/index.html"
+  },
+  {
+    "revision": "0c44f803d6b5f82b13a1",
+    "url": "/add_to_cart/static/css/main.edc01b1d.chunk.css"
+  },
+  {
+    "revision": "3cce2caa28ab9ed79950",
+    "url": "/add_to_cart/static/js/2.9e9e41bd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/add_to_cart/static/js/2.9e9e41bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c44f803d6b5f82b13a1",
+    "url": "/add_to_cart/static/js/main.aad93ff4.chunk.js"
+  },
+  {
+    "revision": "96fd531f83ed3a8314e2",
+    "url": "/add_to_cart/static/js/runtime-main.30444ab3.js"
+  }
+]);
